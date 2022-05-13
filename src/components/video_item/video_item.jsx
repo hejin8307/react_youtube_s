7c8ae8cdca42f559fef6b943e738535d369a1994
limhyejin <hejin8307@naver.com>
2022-05-13 16:12:17 +0900

    video detail

--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -2,25 +2,31 @@ import React from 'react';
 import styles from './video_item.module.css';
 import * as converter from './converter';
 
-const VideoItem = ({video, video: {snippet}}) => (
-  <li className={styles.container}>
-    <div className={styles.video}>
-      <img
-        className={styles.thumbnail}
-        src={snippet.thumbnails.medium.url}
-        alt="thumbnail"
-      />
-      <div className={styles.metadata}>
-        <p className={styles.title}>{snippet.title}</p>
-        <p className={styles.channel}>{snippet.channelTitle}</p>
-        {/* <p className={styles.count}>
+const VideoItem = ({video, video: {snippet}, onVideoClick, display}) => {
+  const displayType = display === 'list' ? styles.list : styles.grid;
+  return (
+    <li
+      className={`${styles.container} ${displayType}`}
+      onClick={() => onVideoClick(video)}
+    >
+      <div className={styles.video}>
+        <img
+          className={styles.thumbnail}
+          src={snippet.thumbnails.medium.url}
+          alt="thumbnail"
+        />
+        <div className={styles.metadata}>
+          <p className={styles.title}>{snippet.title}</p>
+          <p className={styles.channel}>{snippet.channelTitle}</p>
+          {/* <p className={styles.count}>
           {`조회수 ${converter.viewConverter(
             video.statistics.viewCount
           )} ● ${converter.agoConverter(snippet.publishedAt)}`}
         </p> */}
+        </div>
       </div>
-    </div>
-  </li>
-);
+    </li>
+  );
+};
 
 export default VideoItem;
